95a8945c7aa842eeaadb134b7d161e37ba177346
Luis Hernandez <luis.hernandez1@ucalgary.ca>
2025-05-21 02:12:42 -0600

    added buttons commit

--- src/main/java/ca/ucalagry/luis/hernandez1/calc/guiController.java
+++ src/main/java/ca/ucalagry/luis/hernandez1/calc/guiController.java
@@ -1,4 +1,203 @@
 package ca.ucalagry.luis.hernandez1.calc;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+
 public class guiController {
+
+    @FXML
+    private Button clear;
+
+    @FXML
+    private Button delete;
+
+    @FXML
+    private TextArea display;
+
+    @FXML
+    private Button divide;
+
+    @FXML
+    private Button dot;
+
+    @FXML
+    private Button eight;
+
+    @FXML
+    private Button equals;
+
+    @FXML
+    private Button five;
+
+    @FXML
+    private Button four;
+
+    @FXML
+    private Button left;
+
+    @FXML
+    private Button minus;
+
+    @FXML
+    private Button multiply;
+
+    @FXML
+    private Button nine;
+
+    @FXML
+    private Button off;
+
+    @FXML
+    private Button one;
+
+    @FXML
+    private Button percent;
+
+    @FXML
+    private Button plus;
+
+    @FXML
+    private Button plusOrMinus;
+
+    @FXML
+    private Button right;
+
+    @FXML
+    private Button seven;
+
+    @FXML
+    private Button six;
+
+    @FXML
+    private Button three;
+
+    @FXML
+    private Button two;
+
+    @FXML
+    private Button zero;
+
+    @FXML
+    void clear(ActionEvent event) {
+
+    }
+
+    @FXML
+    void delete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void display(MouseEvent event) {
+
+    }
+
+    @FXML
+    void divide(ActionEvent event) {
+
+    }
+
+    @FXML
+    void dot(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eight(ActionEvent event) {
+
+    }
+
+    @FXML
+    void equals(ActionEvent event) {
+
+    }
+
+    @FXML
+    void five(ActionEvent event) {
+
+    }
+
+    @FXML
+    void four(ActionEvent event) {
+
+    }
+
+    @FXML
+    void left(ActionEvent event) {
+
+    }
+
+    @FXML
+    void minus(ActionEvent event) {
+
+    }
+
+    @FXML
+    void multiply(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nine(ActionEvent event) {
+
+    }
+
+    @FXML
+    void off(ActionEvent event) {
+
+    }
+
+    @FXML
+    void one(ActionEvent event) {
+
+    }
+
+    @FXML
+    void percent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void plus(ActionEvent event) {
+
+    }
+
+    @FXML
+    void plusOrMinus(ActionEvent event) {
+
+    }
+
+    @FXML
+    void right(ActionEvent event) {
+
+    }
+
+    @FXML
+    void seven(ActionEvent event) {
+
+    }
+
+    @FXML
+    void six(ActionEvent event) {
+
+    }
+
+    @FXML
+    void three(ActionEvent event) {
+
+    }
+
+    @FXML
+    void two(ActionEvent event) {
+
+    }
+
+    @FXML
+    void zero(ActionEvent event) {
+
+    }
+
 }
